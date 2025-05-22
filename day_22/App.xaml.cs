4ce635ba4e2fcc27619125a248454b830b17bfa5
ezraeffect <build@fx0.kr>
2025-05-22 12:19:51 +0900

    초기 커밋.

---
+++ day_22/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace day_22
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
