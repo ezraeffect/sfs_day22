8b37f4b2a11c591e38709b52c77de53fa8038cd5
ezraeffect <build@fx0.kr>
2025-05-23 09:53:56 +0900

    add: 보충 설명

--- day_22/MainWindow.xaml.cs
+++ day_22/MainWindow.xaml.cs
@@ -30,10 +30,16 @@ using System.Windows.Shapes;
  * - 이미지 파일을 실행 프로그램(.exe)에 포함시켜 배포하는 방식
  * - 프로그램 안에 이미지가 포함 되어 있는 형태라서 외부 파일을 따로 챙길 필요가 없음
  * - 수정은 불가능하지만, 정적인 리소스(아이콘, 배경, 버튼 등)에 매우 적합함
+ * 
+ * [주의]
+ * - WPF에서 Resource 파일 접근 시 반드시 Pack URI 형식 사용 권장.
+ * - XAML에서는 상대경로처럼 보여도 내부적으로 Pack URI로 해석됨.
+ * 
  * [특징]
  * - 배포 간편
  * - 경로 문제 없음
  * - 유지보수 용이
+ * 
  * [사용법]
  * - 솔루션 탐색기에서 프로젝트 우클릭
  * - 추가 -> 기존항목 클릭
@@ -50,7 +56,6 @@ using System.Windows.Shapes;
  * 
  * 2. Content 방식
  * - 이미지 파일을 실행 파일이 있는 폴더에 복사해 놓고, 거기서 직접 불러오는 방식
- * - 사용자가 이미지 업로드 할 때 사용
  * 
  * [특징]
  * - 실행 파일 외부에 따로 존재하는 이미지
@@ -85,6 +90,58 @@ using System.Windows.Shapes;
  * - 이미지 추가
  * - 속성의 '빌드 작업'에서 'Resource'로 변경
  * - '출력 디렉토리에 복사'에서 '복사 안함'
+ * 
+ * 
+ * 
+ * 
+ * [보충설명]
+ * - 앱 개발자가 미리 넣어두는 이미지 -> 빌드 시 포함되어 배포 : Resource, Content
+ * - 앱 실행 중 사용자 입력으로 생긴 이미지 -> 외부 경로에서 접근 : 절대경로(Uri.Absolute)
+ * 
+ * [Resource VS Content]
+ * Resource
+ * - 빌드 결과 : 어셈블리 안에 내장 됨 (Assamblyinfo.cs)
+ * - 경로 접근 : pack URI 방식
+ * - 파일 존재 위치 : 실행중에는 실제 파일 없음 (메모리 상에 존재)
+ * - 수정/삭제 : 불가능(읽기 전용)
+ * - 대표 용도 : 아이콘, UI 배경 이미지, 버튼 배경 이미지 등 Static Resource
+ * 
+ * Content
+ * - 빌드 결과 : 실행파일 옆에 복사 됨
+ * - 경로 접근 : 상대/절대 경로
+ * - 파일 존재 위치 : 실행 폴더에 파일이 있음
+ * - 수정/삭제 : 가능
+ * - 대표 용도 : 설명 이미지, 문서, 동영상 등 실제 파일이 필요할 때
+ *  ㄴ 실행 중 수정 가능 -> 유저 설정 바꿀 떄 (ex. 게임 세이브 데이터 저장)
+ *  ㄴ 용량 이슈 / 관리 용이성
+ *      ㄴ Resource : 실행파일 크기가 점점 커짐
+ *      ㄴ Content : 실행파일이 가볍고, 메모리 사용도 가벼워짐 -> 파일은 옆에 따로 존재
+ *          ㄴ 유지보수 시 Content는 파일만 바꾸면 되므로 유리
+ * 
+ * [Build Action vs Urikind]
+ * - Resource, Content는 "파일의 빌드 방식"에 대한 설정
+ * - Urikind는 해당 파일을 어떻게 접근할지에 대한 설정 방식
+ *  => 둘은 직접적인 관련이 없음
+ *  
+ *  Resource - Pack URI => Urikind.absolute
+ *  Content - 상대/절대 경로 => Urikind.absolute, Relative
+ *  (외부 파일) - 물리 경로 or URL => 보통 absolute 사용
+ *  
+ *  [Resource를 Pack URI로 써야하는 이유?]
+ *  - ..exe 안에 있는 리소스 파일을 찾을 수 있도록 Pack URI라는 전용 주소 체계를 사용하는 것
+ *  
+ *  [@]
+ *  - 이스케이스 시퀀스 무시
+ *  => 일반 문자열에서 백슬래시가 이스크에프 문자로 사용되는데 파일 경로에서는 경로 구분자로 사용되기 떄문에 백슬래시를 두 번 입력해야함
+ *      @을 사용하면 백슬래시를 한번만 입력해도 됨. @가 붙은 문자열은 백슬래시를 이스케이프 시퀀스로 해석하지 않고 일반 문자로 해석함
+ *  - 보통 절대경로에서 사용
+ *  
+ *  [escape sequence]
+ *  - 프로그래밍 언어나 텍스트 형식에서 특수한 의미를 가지는 문자를 일반 문자처럼 취급, 특수 문자를 표현하기 위해 사용되는 문자
+ *  - 보통 백슬래시와 함께 사용되어 일련의 이스케이프 시퀀스를 만듦.
+ *  
+ *  => 문자열 내에서 특수문자를 표현하기 위해, ""가 문자열의 시작과 끝을 나타내듯
+ *      \ 도 출력시 특별한 동작을 지시하기 위해서 사용
  */
 namespace day_22
 {
@@ -95,7 +152,7 @@ namespace day_22
     {
         private bool isAngry = true;
 
-        private Uri uriAngryImage = new Uri("pack://application:,,,/Assets/6.png",UriKind.Absolute);
+        private Uri uriAngryImage = new Uri("pack://application:,,,/Assets/6.png", UriKind.Absolute);
         private Uri uriHappyImage = new Uri("pack://application:,,,/Assets/5.png", UriKind.Absolute);
         public MainWindow()
         {
@@ -111,7 +168,7 @@ namespace day_22
              * UriKind
              * - Relative: 실행 파일 기준 경로
              * - Absolute: 전체 경로 명시
-             * - RelativeOrAbsolute: 둘 중 맞는걸 자동 판멸
+             * - RelativeOrAbsolute: 둘 중 맞는걸 자동 판정
              * 
              * new BitmapImage(...) - Uri를 통해 실제 이미지 객체 생성
              */
